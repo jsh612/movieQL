5e143ff067722b61890e91a3fce13f05528307a4
seungho Jeon <urijsh612@gmail.com>
2019-11-07 17:10:34 +0900

    #4 Creating the first Query and Resolver

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const reslovers = {
+  Query: {
+    name: () => "손흥민"
+  }
+};
+
+export default reslovers;
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  // typeDefs : GraphQL type definitions
+  typeDefs: "graphql/schema.graphql",
+  resolvers
+});
 
 server.start(() => console.log("GraphQL 서버가 시작되었다"));
