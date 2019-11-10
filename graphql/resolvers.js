a62a167e73f0e9d9aa4cf29214d8cc48250ffadd
seungho Jeon <urijsh612@gmail.com>
2019-11-10 17:13:52 +0900

    #8 Defining Mutations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,17 @@
-import { people, getByid } from "./db";
+import { getByid, getMovies } from "./db";
 
 const reslovers = {
   // data를 보내는 역할
   Query: {
-    people: () => people,
-    // person: (_, arg) => {
+    movies: () => getMovies(),
+    // movie: (_, arg) => {
     //   console.log(arg); // { id: 1} 객체 혀앹로 출력
     // }
-    // person: (_, arg) => getByid(arg.id)
-    person: (_, { id }) => getByid(id)
+    // movie: (_, arg) => getByid(arg.id)
+    movie: (_, { id }) => getByid(id)
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score)
   }
 };
 
