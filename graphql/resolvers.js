0672be5315593a8f6245601861472164b9bf4a6e
seungho Jeon <urijsh612@gmail.com>
2019-11-11 00:42:42 +0900

    "마지막 추가적 사항"

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,17 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const reslovers = {
   // data를 보내는 역할
   Query: {
-    movies: (_, { rating, limit }) => getMovies(rating, limit)
+    movies: (_, { rating, limit }) => getMovies(rating, limit),
     // movie: (_, arg) => {
     //   console.log(arg); // { id: 1} 객체 혀앹로 출력
     // }
     // movie: (_, arg) => getByid(arg.id)
     // movie: (_, { id }) => getByid(id)
+
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 };
 
