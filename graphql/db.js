ba362478965e59cd9477661be0def9c2902f5421
seungho Jeon <urijsh612@gmail.com>
2019-11-11 10:54:56 +0900

    복습

--- graphql/db.js
+++ graphql/db.js
@@ -5,7 +5,7 @@ const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = async (limit, rating) => {
+export const getMovies = async (rating, limit) => {
   const {
     data: {
       data: { movies }
